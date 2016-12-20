d31a20ad7ae356ec1f67777f9dc0573990355c0e
vinh huynh <vvvhuynh@yahoo.com>
2016-12-20 11:30:44 -0600

    added functionality to create items

---
+++ public/javascripts/app.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function(){
+  var searchItem;
+
+  // ajaxRequest(topic);
+  $('#btn-search').on('click', function(e){
+    e.preventDefault();
+    searchItem = $('#item-search').val();
+    console.log(searchItem);
+    ajaxRequest(searchItem)
+  });
+
+  function ajaxRequest(searchItem) {
+      var itemRequest = $.ajax({
+        url: 'wishlist-api',
+        method: 'POST',
+        data: {
+            // keywords: $('#item-search').val()
+            keywords: searchItem
+        }
+      });
+      itemRequest.done(function(response){
+        console.log(response);
+        console.log('Label: ', response[0].ItemAttributes[0].Label[0]);
+        console.log('Price: ', response[0].ItemAttributes[0].ListPrice[0].FormattedPrice[0]);
+        console.log('ImageURL: ', response[0].MediumImage[0].URL[0]);
+      });
+    }
+});
+
+  function populateContent(response) {
+    var results = response.data.children;
+
+      // Forms the html statement that gets added into the DOM
+      var elements = ['<li class=\"contentItem\">',
+                        '<div class="row">',
+                          '<div class="col-md-1">',
+                            '<h4>' + results[i].data.score + '</h4>',
+                          '</div>',
+                          '<div class="col-md-3">',
+                            '<img class=\"thumbnails\" src=\"' + (brokenImage ? 'images/RedditSnoo.png' : thumbNail) + '\"/>',
+                          '</div>',
+                          '<div class="col-md-8">',
+                            '<h4 class=\"contentTitle\">' + title + '<h4/>',
+                          '</div>',
+                        '</div>',
+                      '</li>'
+                    ].join('');
+      $('#main-content').append(elements);
+    }
+  } //end of populateContent function
+});
--- routes/teachers.js
+++ routes/teachers.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var amazon = require('amazon-product-api');
+
+var client = amazon.createClient({
+  awsId: process.env.ACCESS_KEY_ID,
+  awsSecret: process.env.SECRET_ACCESS_KEY,
+  awsTag: process.env.AWS_TAG
+});
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -12,13 +21,31 @@ router.get('/tlanding', function(req, res, next) {
   res.render('tlanding');
 });
 
+//--------------------------------------------------
+
 router.get('/wishlist-new', function(req, res, next) {
-  // res.send('TEACHER make a NEW WISHLIST goes here');
   res.render('wishlist-new');
 });
 
-router.get('/wishlist-inprog', function(req, res, next) {
-  res.send('TEACHER make IN-PROGRESS WISHLIST goes here');
+router.post('/wishlist-api', function(req, res, next) {
+  client.itemSearch({
+    keywords: req.body.keywords,
+    responseGroup: 'ItemAttributes,Offers,Images'
+  }).then(function(results){
+    res.json(results);
+  }).catch(function(err){
+    console.log('Catch: ');
+    console.log(err[0].Error);
+    res.send(err[0].Error);
+  });
+
+
+});
+
+router.get('/mywishlists', function(req, res, next) {
+  // res.send('My Wishlists goes here')
+
+  res.render('mywishlists');
 });
 
 module.exports = router;
